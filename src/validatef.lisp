5fbfe4905770d7aa9230085535b2a4ebe0f58a1f
Shinichi Sato <hyotang.karakoma@gmail.com>
2021-11-19 15:53:40 +0900

    Add: VALIDATEF.

--- src/validatef.lisp
+++ src/validatef.lisp
@@ -2,6 +2,22 @@
 
 (defpackage :validatef
   (:use :cl)
-  (:export))
+  (:export #:validatef))
 
 (in-package :validatef)
+
+(defmacro validatef (&body clauses)
+  `(progn
+    ,@(loop :for (place key on-fail) :on clauses :by #'cdddr
+            :with symbol := (gensym "VALUE")
+            :collect `(handler-case (values ,key)
+                        (error ()
+                          ,on-fail)
+                        ,@(when place
+                            `((:no-error (,symbol) (setf ,place ,symbol))))))))
+
+(defun pprint-validatef (stream exp)
+  (setf stream (or stream *standard-output*))
+  (funcall (formatter "~:<~W~^~1I ~_~@{~W~^ ~:I~W~^ ~:_~W~^ ~1I~_~}~:>") stream
+           exp))
+
